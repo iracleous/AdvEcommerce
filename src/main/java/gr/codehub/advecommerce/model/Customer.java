85ddf5d0a55d19531bb57c8a04d8c6d707d89ae8
dimitris <iracleous@gmail.com>
2021-12-08 09:49:35 +0200

    GUI minor corrections

--- src/main/java/gr/codehub/advecommerce/model/Customer.java
+++ src/main/java/gr/codehub/advecommerce/model/Customer.java
@@ -2,10 +2,18 @@ package gr.codehub.advecommerce.model;
 
 import lombok.Data;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import java.time.LocalDate;
 
 @Data
+@Entity
 public class Customer {
+
+    @Id
+    @GeneratedValue(strategy= GenerationType.AUTO)
     private int id;
     private String name;
     private String email;
---
+++ src/main/java/gr/codehub/advecommerce/testing/TestingMain.java
@@ -0,0 +1,31 @@
+package gr.codehub.advecommerce.testing;
+
+import gr.codehub.advecommerce.jpautil.JpaUtil;
+import gr.codehub.advecommerce.model.Customer;
+
+import javax.persistence.EntityManager;
+import java.time.LocalDate;
+
+public class TestingMain {
+
+    public static void main(String[] args) {
+        EntityManager em = JpaUtil.getEntityManager();
+
+        Customer customer = new Customer();
+        customer.setName("George");
+        customer.setDateOfBirth(LocalDate.of(2000,1,1));
+
+
+        System.out.println("Id = "+customer.getId());
+        em.getTransaction().begin();
+        em.persist(customer);
+        em.getTransaction().commit();
+        System.out.println("Id = "+customer.getId());
+
+
+        em.close();
+        JpaUtil.shutdown();
+        System.out.println("end");
+
+    }
+}
